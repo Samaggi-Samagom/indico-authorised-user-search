143d6ea7c0e8a562c02631728fa7f5f425219cdd
Panawat Wong-klaew <panawat_vista@hotmail.com>
2020-07-29 19:37:16 +0700

    Configure logger

--- indico_authorised_user_search/plugin.py
+++ indico_authorised_user_search/plugin.py
@@ -6,13 +6,16 @@ import indico.modules.users.util
 from flask import session
 
 
-def add_monkey_patch(logger):
+def add_monkey_patch():
 
     def authorised_search_users(**kwargs):
-        logger.info("session: {}".format(session))
-        raise Exception("Just wanna see the traceback")
-        indico.modules.users.util.search_users(**kwargs)
+        logger = Logger.get("search_users")
+        logger.info("Session: {}".format(session))
+        # raise Exception("Just wanna see the traceback")
+        return old_search_users(**kwargs)
 
+    AuthorisedUserSearchPlugin.logger.info("adding monkey patch")
+    old_search_users = indico.modules.users.util.search_users
     indico.modules.users.util.search_users = authorised_search_users
 
 
@@ -26,4 +29,4 @@ class AuthorisedUserSearchPlugin(IndicoPlugin):
     def init(self):
         super(AuthorisedUserSearchPlugin, self).init()
         AuthorisedUserSearchPlugin.logger.info("New3 Plugin Init")
-        add_monkey_patch(AuthorisedUserSearchPlugin.logger)
+        add_monkey_patch()
