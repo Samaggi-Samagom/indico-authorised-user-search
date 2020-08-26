6206fc51da31edab8016a4e5ea1d3380bcc4f8a3
Panawat Wong-klaew <panawat_vista@hotmail.com>
2020-08-26 22:37:18 +0700

    Implement settings and permission check

--- indico_authorised_user_search/plugin.py
+++ indico_authorised_user_search/plugin.py
@@ -2,22 +2,39 @@
 # See https://github.com/indico/indico/blob/master/indico/core/plugins/__init__.py#L48 for details
 # about the IndicoPlugin class.
 from indico.core.plugins import IndicoPlugin  # , IndicoPluginBlueprint, url_for_plugin
-from indico.core.logger import Logger
+# from indico.core.logger import Logger
+from indico.modules.groups.core import GroupProxy
 import indico.modules.users.util
+
 from flask import session
+
+from indico.web.forms.base import IndicoForm
+from wtforms.fields import IntegerField
+from wtforms.validators import InputRequired, NumberRange
 
 
 def add_monkey_patch():
 
+    def get_user_search_permission():
+        group_proxy = GroupProxy(AuthorisedUserSearchPlugin.settings.get('group_id'))
+        return session.user in group_proxy
+
     def authorised_search_users(**kwargs):
-        logger = Logger.get("search_users")
-        logger.info("Session: {}".format(session))
+        # logger = Logger.get("search_users")
+        # logger.info("Session: {}".format(session))
         # raise Exception("Just wanna see the traceback")
-        return old_search_users(**kwargs)
+        if get_user_search_permission():
+            return old_search_users(**kwargs)
+        else:
+            return set(session.user)
 
     AuthorisedUserSearchPlugin.logger.info("adding monkey patch")
     old_search_users = indico.modules.users.util.search_users
     indico.modules.users.util.search_users = authorised_search_users
+
+
+class AuthorisedGroupSettings(IndicoForm):
+    group_id = IntegerField('Local Group Id', [InputRequired(), NumberRange(min=1)])
 
 
 class AuthorisedUserSearchPlugin(IndicoPlugin):
@@ -27,7 +44,13 @@ class AuthorisedUserSearchPlugin(IndicoPlugin):
     user searching
     """
 
+    configurable = True
+    settings_form = AuthorisedGroupSettings
+    default_settings = {
+        'group_id': None
+    }
+
     def init(self):
         super(AuthorisedUserSearchPlugin, self).init()
-        AuthorisedUserSearchPlugin.logger.info("New3 Plugin Init")
+        AuthorisedUserSearchPlugin.logger.info("New4 Plugin Init")
         add_monkey_patch()
