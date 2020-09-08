0e5bbdd5b95e91e70f99292651c12a493f49244e
Panawat Wong-kleaw <panawat_vista@hotmail.com>
2020-09-08 22:54:19 +0700

    Release 0.1.0 (#3)

--- indico_authorised_user_search/plugin.py
+++ indico_authorised_user_search/plugin.py
@@ -1,16 +1,87 @@
-# from indico.core import signals
 # See https://github.com/indico/indico/blob/master/indico/core/plugins/__init__.py#L48 for details
 # about the IndicoPlugin class.
 from indico.core.plugins import IndicoPlugin  # , IndicoPluginBlueprint, url_for_plugin
+# from indico.core.logger import Logger
+from indico.modules.groups.core import GroupProxy
+
+import indico.modules.users.util
+import indico.legacy.services.implementation.search
+
+from flask import session
+
+from indico.web.forms.base import IndicoForm
+from wtforms.fields import IntegerField
+from wtforms.validators import InputRequired, NumberRange
+
+
+def get_user_search_permission():
+    """Check whether the current user is allowed to search users or not"""
+    if session.user.is_blocked:
+        return False
+
+    if session.user.is_admin:
+        return True
+
+    group_id = AuthorisedUserSearchPlugin.settings.get('group_id')
+    if group_id is None:
+        return True
+
+    group_id = int(group_id)
+    if 0 == group_id:
+        return True
+    elif group_id < 0:
+        return False
+    else:
+        group_proxy = GroupProxy(group_id)
+        return session.user in group_proxy
+
+
+def add_monkey_patch():
+
+    def authorised_search_users(**kwargs):
+        if get_user_search_permission():
+            return old_search_users(**kwargs)
+        else:
+            return set([session.user])
+
+    AuthorisedUserSearchPlugin.logger.info("Adding monkey patch")
+
+    old_search_users = indico.modules.users.util.search_users
+    indico.modules.users.util.search_users = authorised_search_users
+
+    indico.legacy.services.implementation.search.methodMap["users"] = NewRPCSearchUsers
+
+
+class NewRPCSearchUsers(indico.legacy.services.implementation.search.SearchUsers):
+
+    def _process_args(self):
+        super(NewRPCSearchUsers, self)._process_args()
+        if not get_user_search_permission():
+            self._event = None
+
+
+class AuthorisedGroupSettings(IndicoForm):
+    group_id = IntegerField('Local Group Id', [InputRequired(), NumberRange(min=-1)])
 
 
 class AuthorisedUserSearchPlugin(IndicoPlugin):
     """Authorised User Search
 
-    Require the users to be authorised to use the user search functionality before they can perform
-    user searching
+    Only allow users in the specified local group to search for other users.
+    Users who are not in the specified group will always see only themselves when they perform user
+    searching.
+
+    Specify group_id=0 to allow everyone, and group_id=-1 to block everyone.
+
+    Admins can always search for other users.
     """
+
+    configurable = True
+    settings_form = AuthorisedGroupSettings
+    default_settings = {
+        'group_id': None
+    }
 
     def init(self):
         super(AuthorisedUserSearchPlugin, self).init()
-        AuthorisedUserSearchPlugin.logger.info("New3 Plugin Init")
+        add_monkey_patch()
--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='indico-authorised-user-search',
-    version='0.0.1-dev',
+    version='0.1.dev1',
     author='Samaggi Samagom',
+    author_email='tech@samaggisamagom.com',
+    description='An Indico 2 plugin for allowing only users in a single group to search for other users',
     packages=find_packages(),
     install_requires=[
         'indico>=2.0'
